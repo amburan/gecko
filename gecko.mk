d92c5bbd6d4821dfe09e867ac6e22345204d5f41
Stephen Thomas <stephenthomas1@boisestate.edu>
2015-07-14 14:35:59 -0600

    Making changes to gecko.mk file to allow calling LAMMPS library (.a file) from Gecko app.

--- gecko.mk
+++ gecko.mk
@@ -1,2 +1,2 @@
 app_INCLUDES += -I /Users/sthomas/lammps/src
-ADDITIONAL_LIBS := /Users/sthomas/lammps/src/liblammps_mpi.a /Users/sthomas/lammps/lib/atc/libatc.a  -lblas -llapack -lpthread -lgfortran
+ADDITIONAL_LIBS := -L/Users/sthomas/lammps/src -llammps_mpi /Users/sthomas/lammps/lib/atc/libatc.a  -lblas -llapack -lpthread -lgfortran
